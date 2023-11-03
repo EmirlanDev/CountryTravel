c18f83dbf8035692ddde527507783581399a9839
EmirlanDev <147941140+EmirlanDev@users.noreply.github.com>
2023-11-03 14:32:20 +0600

    J-A (#5)

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -65,7 +65,7 @@ const Header = () => {
                     </NavLink>
                     <NavLink to="/regions/jalalAbad">
                       <p style={{ display: regions ? "" : "none" }}>
-                        {/* Jalal-Abad */}
+                        Jalal-Abad
                       </p>
                     </NavLink>
                     <NavLink to="/regions/osh">
