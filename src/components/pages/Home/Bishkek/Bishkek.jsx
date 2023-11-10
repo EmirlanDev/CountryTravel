83d15cbbbbabfe4179f264e9c8897764bb94a415
EmirlanDev <frdevemirlan@gmail.com>
2023-11-10 12:56:40 +0600

    bishkek

--- src/components/pages/Home/Bishkek/Bishkek.jsx
+++ src/components/pages/Home/Bishkek/Bishkek.jsx
@@ -1,9 +1,27 @@
 import React from "react";
+import bishkek from "./.././../././../../images/bishkekB.png";
 
 const Bishkek = () => {
   return (
-    <div>
-      <h1>Bishkek</h1>
+    <div id="bishkek">
+      <div className="container">
+        <h2>Bishkek</h2>
+        <div className="bishkek">
+          <div className="bishkek__img">
+            <img src={bishkek} alt="bishkek" />
+          </div>
+          <div className="bishkek__text">
+            <h1>Bishkek</h1>
+            <p>
+              This is the capital of a picturesque mountainous country -
+              Kyrgyzstan, from where tourists make trips to the world-famous,
+              crystal-clear Issyk-Kul and other natural attractions of the Tien
+              Shan. Framed by mountains, the city is rich in beautiful views,
+              monumental monuments, parks and museum antiquities.
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
