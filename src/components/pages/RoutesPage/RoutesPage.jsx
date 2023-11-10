463d8b23ef9b06694d40abb5a6eeb24b2344c145
EmirlanDev <frdevemirlan@gmail.com>
2023-11-10 15:56:46 +0600

    pages

---
+++ src/components/pages/RoutesPage/RoutesPage.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const RoutesPage = () => {
+  return (
+    <div id="routes">
+      <div className="container">
+        <div className="routes">
+          <h1>Routes</h1>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default RoutesPage;
